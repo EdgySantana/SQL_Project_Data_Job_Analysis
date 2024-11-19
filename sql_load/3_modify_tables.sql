8d7737d4d18480ff8df5993511c152a92908c207
Edmundo Santana <79051090+EdgySantana@users.noreply.github.com>
2024-11-19 09:48:01 -0500

    Update 3_modify_tables.sql

--- sql_load/3_modify_tables.sql
+++ sql_load/3_modify_tables.sql
@@ -25,19 +25,3 @@ NOTE: If you are having issues with permissions. And you get error:
 
 -- NOTE: This has been updated from the video to fix issues with encoding
 
-
-COPY company_dim
-FROM '/Users/administrator/Documents/SQL_Project_Data_Job_Analysis/csv_files/company_dim.csv'
-WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
-
-COPY skills_dim
-FROM '/Users/administrator/Documents/SQL_Project_Data_Job_Analysis/csv_files/skills_dim.csv'
-WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
-
-COPY job_postings_fact
-FROM '/Users/administrator/Documents/SQL_Project_Data_Job_Analysis/csv_files/job_postings_fact.csv'
-WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
-
-COPY skills_job_dim
-FROM '/Users/administrator/Documents/SQL_Project_Data_Job_Analysis/csv_files/skills_job_dim.csv'
-WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
