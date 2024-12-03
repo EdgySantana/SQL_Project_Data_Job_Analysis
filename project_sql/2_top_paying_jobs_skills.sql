9ea472498a87f3ec69af9498e5e2845578b32943
Edgy San <79051090+EdgySantana@users.noreply.github.com>
2024-12-03 12:02:04 -0500

    Add images, graphs & files updates.

--- project_sql/2_top_paying_jobs_skills.sql
+++ project_sql/2_top_paying_jobs_skills.sql
@@ -8,7 +8,7 @@ helping job seekers understand which skills to develop that align with top salar
 
 
 WITH top_paying_jobs AS (
-    SELECT
+    SELECT	
         job_id,
         job_title,
         salary_year_avg,
@@ -17,8 +17,8 @@ WITH top_paying_jobs AS (
         job_postings_fact
     LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
     WHERE
-        job_title_short = 'Data Analyst' AND
-        job_location = 'Anywhere' AND
+        job_title_short = 'Data Analyst' AND 
+        job_location = 'Anywhere' AND 
         salary_year_avg IS NOT NULL
     ORDER BY
         salary_year_avg DESC
@@ -27,15 +27,18 @@ WITH top_paying_jobs AS (
 
 SELECT 
     top_paying_jobs.*,
-    skills_dim.skills
-FROM 
-    top_paying_jobs  
+    skills
+FROM top_paying_jobs
 INNER JOIN skills_job_dim ON top_paying_jobs.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
 ORDER BY
-    salary_year_avg DESC
+    salary_year_avg DESC;
 
 /*
+
+ChatGPT Prompt: These are the top 10 Data analyst roles I found in job postings in 2023. Can you analyze
+the skill column and display insights
+
 Here is the breakdown of the most in demand skills for data analysts in 2023, based on the job postings:
 
 SQL is leading with a bold count of 8.
@@ -69,7 +72,33 @@ Other skills like R, SnowFlake, Pandas, and Excell show varying degrees of deman
     "company_name": "AT&T",
     "skills": "r"
   },
-  {
+  {WITH top_paying_jobs AS (
+    SELECT
+        job_id,
+        job_title,
+        salary_year_avg,
+        name AS company_name
+    FROM
+        job_postings_fact
+    LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+    WHERE
+        job_title_short = 'Data Analyst' AND
+        job_location = 'Anywhere' AND
+        salary_year_avg IS NOT NULL
+    ORDER BY
+        salary_year_avg DESC
+    LIMIT 10
+)
+
+SELECT 
+    top_paying_jobs.*,
+    skills_dim.skills
+FROM 
+    top_paying_jobs  
+INNER JOIN skills_job_dim ON top_paying_jobs.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+ORDER BY
+    salary_year_avg DESC
     "job_id": 552322,
     "job_title": "Associate Director- Data Insights",
     "salary_year_avg": "255829.5",
